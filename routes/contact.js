5a5774c81bfb8e02a18a782216e2e7399195b762
Ferran Basora <fcsonline@gmail.com>
2016-06-19 23:51:06 +0200

    Backend for contact route

---
+++ routes/contact.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var router = express.Router();
+var domain = 'hatbarcelona.com';
+var mailgun = require('mailgun-js')({apiKey: process.env.MAILGUN_API_KEY, domain: domain});
+
+router.post('/contact', function(req, res) {
+
+  if (!req.body) {
+    res.sendStatus(409);
+    return;
+  }
+
+  req.app.render('contact', req.body, function(err, html) {
+    if (err) {
+      res.sendStatus(409);
+      return;
+    }
+
+    var data = {
+      from: req.body.name + ' <' + req.body.email + '>',
+      to: 'info@hatbarcelona.com',
+      bcc: 'fcsonline@gmail.com',
+      subject: req.body.subject,
+      html: html
+    };
+
+    mailgun.messages().send(data, function (err, body) {
+      if (err) {
+        res.sendStatus(409);
+        return;
+      }
+
+      res.json({});
+    });
+  });
+  
+});
+
+module.exports = router;
