70996a9081e31fb50c05b1bd37a6b92a5edc66ed
Ferran Basora <fcsonline@gmail.com>
2016-06-20 22:07:41 +0200

    Mount properly contact endpoint

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../hatbarcelona.website.static/_site')));
 
-app.use('/contact', contact);
+app.use('/', contact);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
